da3396a454221e2f6de430378286e421ccb8d16a
AdarAvsian <aavsian@gmail.com>
2024-10-10 11:11:09 -0400

    test3

--- scripts/measurements.py
+++ scripts/measurements.py
@@ -1,147 +1,147 @@
-import os
-import tempfile
-from PIL import Image
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-import boto3
-import keras
+# import os
+# import tempfile
+# from PIL import Image
+# import numpy as np
+# import tensorflow as tf
+# from tensorflow.keras.models import load_model
+# import boto3
+# import keras
 
-class Scalar2Gaussian():
-    def __init__(self,min=0.0,max=99.0,sigma=4.0,bins=1000):
-        self.min, self.max, self.bins, self.sigma = float(min), float(max), bins, sigma
-        self.idxs = np.linspace(self.min,self.max,self.bins)
-    def softmax(self, vector):
-        e_x = np.exp(vector - np.max(vector))
-        return e_x / e_x.sum()
+# class Scalar2Gaussian():
+#     def __init__(self,min=0.0,max=99.0,sigma=4.0,bins=1000):
+#         self.min, self.max, self.bins, self.sigma = float(min), float(max), bins, sigma
+#         self.idxs = np.linspace(self.min,self.max,self.bins)
+#     def softmax(self, vector):
+#         e_x = np.exp(vector - np.max(vector))
+#         return e_x / e_x.sum()
 
-    def code(self,scalar):
-        probs = np.exp(-((self.idxs - scalar) / 2*self.sigma)**2)
-        probs = probs/probs.sum()
-        return probs
+#     def code(self,scalar):
+#         probs = np.exp(-((self.idxs - scalar) / 2*self.sigma)**2)
+#         probs = probs/probs.sum()
+#         return probs
   
-    def decode(self, vector):
-        if np.abs(vector.sum()-1.0) < 1e-3 and np.all(vector>-1e-4):
-            probs=vector
-        else: 
-            probs = self.softmax(vector)
-        scalar = np.dot(probs, self.idxs)
-        return scalar
+#     def decode(self, vector):
+#         if np.abs(vector.sum()-1.0) < 1e-3 and np.all(vector>-1e-4):
+#             probs=vector
+#         else: 
+#             probs = self.softmax(vector)
+#         scalar = np.dot(probs, self.idxs)
+#         return scalar
 
-    def decode_tensor(self, vector):
-        def true_fn():
-            return vector
+#     def decode_tensor(self, vector):
+#         def true_fn():
+#             return vector
 
-        def false_fn():
-            return tf.nn.softmax(vector)
+#         def false_fn():
+#             return tf.nn.softmax(vector)
 
-        probs = tf.cond(
-            tf.logical_and(
-                tf.math.abs(tf.reduce_sum(vector) - 1.0) < 1e-3,
-                tf.reduce_all(vector > -1e-4)
-            ),
-            true_fn,
-            false_fn
-        )
-        scalar = tf.reduce_sum(probs * self.idxs)
-        return scalar
+#         probs = tf.cond(
+#             tf.logical_and(
+#                 tf.math.abs(tf.reduce_sum(vector) - 1.0) < 1e-3,
+#                 tf.reduce_all(vector > -1e-4)
+#             ),
+#             true_fn,
+#             false_fn
+#         )
+#         scalar = tf.reduce_sum(probs * self.idxs)
+#         return scalar
   
-s2g = {
-    'right_bicep': Scalar2Gaussian(min=20.0, max=60.0),
-    'left_bicep': Scalar2Gaussian(min=20.0, max=60.0),
-    'chest': Scalar2Gaussian(min=60.0, max=170.0),
-    'right_forearm': Scalar2Gaussian(min=15.0, max=40.0),
-    'left_forearm': Scalar2Gaussian(min=15.0, max=40.0),
-    'right_quad': Scalar2Gaussian(min=40.0, max=70.0),
-    'left_quad': Scalar2Gaussian(min=40.0, max=70.0),
-    'right_calf': Scalar2Gaussian(min=20.0, max=60.0),
-    'left_calf': Scalar2Gaussian(min=20.0, max=60.0),
-    'waist': Scalar2Gaussian(min=70.0, max=140.0),
-    'hips': Scalar2Gaussian(min=80.0, max=110.0)
-}
+# s2g = {
+#     'right_bicep': Scalar2Gaussian(min=20.0, max=60.0),
+#     'left_bicep': Scalar2Gaussian(min=20.0, max=60.0),
+#     'chest': Scalar2Gaussian(min=60.0, max=170.0),
+#     'right_forearm': Scalar2Gaussian(min=15.0, max=40.0),
+#     'left_forearm': Scalar2Gaussian(min=15.0, max=40.0),
+#     'right_quad': Scalar2Gaussian(min=40.0, max=70.0),
+#     'left_quad': Scalar2Gaussian(min=40.0, max=70.0),
+#     'right_calf': Scalar2Gaussian(min=20.0, max=60.0),
+#     'left_calf': Scalar2Gaussian(min=20.0, max=60.0),
+#     'waist': Scalar2Gaussian(min=70.0, max=140.0),
+#     'hips': Scalar2Gaussian(min=80.0, max=110.0)
+# }
 
-@keras.saving.register_keras_serializable()
-def l1_l2_loss(y_true, y_pred, output):
-    l1_loss = tf.keras.losses.categorical_crossentropy(y_true, y_pred)  # Softmax Crossentropy loss
-    decoded_y_true = s2g[output].decode_tensor(vector=y_true)
-    decoded_y_pred = s2g[output].decode_tensor(vector=y_pred)
-    # Ensure decoded outputs have at least one dimension
-    decoded_y_true = tf.expand_dims(decoded_y_true, -1)
-    decoded_y_pred = tf.expand_dims(decoded_y_pred, -1)
+# @keras.saving.register_keras_serializable()
+# def l1_l2_loss(y_true, y_pred, output):
+#     l1_loss = tf.keras.losses.categorical_crossentropy(y_true, y_pred)  # Softmax Crossentropy loss
+#     decoded_y_true = s2g[output].decode_tensor(vector=y_true)
+#     decoded_y_pred = s2g[output].decode_tensor(vector=y_pred)
+#     # Ensure decoded outputs have at least one dimension
+#     decoded_y_true = tf.expand_dims(decoded_y_true, -1)
+#     decoded_y_pred = tf.expand_dims(decoded_y_pred, -1)
     
-    l2_loss = tf.keras.losses.MeanSquaredError()(decoded_y_true, decoded_y_pred)  # MSE loss
-    total_loss = l1_loss + l2_loss
-    return total_loss
+#     l2_loss = tf.keras.losses.MeanSquaredError()(decoded_y_true, decoded_y_pred)  # MSE loss
+#     total_loss = l1_loss + l2_loss
+#     return total_loss
 
-def loss_wrapper(output):
-    def loss_fn(y_true, y_pred):
-        return l1_l2_loss(y_true, y_pred, output)
+# def loss_wrapper(output):
+#     def loss_fn(y_true, y_pred):
+#         return l1_l2_loss(y_true, y_pred, output)
     
-custom_objects = {
-    'l1_l2_loss': l1_l2_loss
-}
+# custom_objects = {
+#     'l1_l2_loss': l1_l2_loss
+# }
 
-model_dir = '/code/files'
-model_path = os.path.join(model_dir, 'model.keras')
-if not os.path.exists(model_dir):
-    os.makedirs(model_dir)
+# model_dir = '/code/files'
+# model_path = os.path.join(model_dir, 'model.keras')
+# if not os.path.exists(model_dir):
+#     os.makedirs(model_dir)
 
-if not os.path.exists(model_path):
-    bucket_name = 'ml-models-kino'
-    file_key = 'contralateral-measurements/model.keras'
-    local_file_name = model_path
+# if not os.path.exists(model_path):
+#     bucket_name = 'ml-models-kino'
+#     file_key = 'contralateral-measurements/model.keras'
+#     local_file_name = model_path
 
-    # Download the .keras file from S3
-    s3 = boto3.client(
-        's3',
-        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-    )
-    s3.download_file(bucket_name, file_key, local_file_name)
+#     # Download the .keras file from S3
+#     s3 = boto3.client(
+#         's3',
+#         aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+#         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+#     )
+#     s3.download_file(bucket_name, file_key, local_file_name)
 
-def preprocess(frontImage, backImage, weight, height, gender, demographic):
-    frontImage = frontImage.resize((224, 224)).convert('RGB')
-    backImage = backImage.resize((224, 224)).convert('RGB')
+# def preprocess(frontImage, backImage, weight, height, gender, demographic):
+#     frontImage = frontImage.resize((224, 224)).convert('RGB')
+#     backImage = backImage.resize((224, 224)).convert('RGB')
     
-    frontImage = np.array(frontImage)
-    backImage = np.array(backImage)
+#     frontImage = np.array(frontImage)
+#     backImage = np.array(backImage)
     
-    gender_map = {'male': 0, 'female': 1}
-    demographic_map = {"white": 0, "black": 1, "asian": 2, "hispanic": 3}
+#     gender_map = {'male': 0, 'female': 1}
+#     demographic_map = {"white": 0, "black": 1, "asian": 2, "hispanic": 3}
 
-    tabular_data = np.array([
-        int(weight), 
-        int(height), 
-        gender_map[gender],
-        demographic_map[demographic]
-    ])
+#     tabular_data = np.array([
+#         int(weight), 
+#         int(height), 
+#         gender_map[gender],
+#         demographic_map[demographic]
+#     ])
     
-    # Ensure all inputs have a batch dimension
-    frontImage = np.expand_dims(frontImage, axis=0)
-    backImage = np.expand_dims(backImage, axis=0)
-    tabular_data = np.expand_dims(tabular_data, axis=0)
+#     # Ensure all inputs have a batch dimension
+#     frontImage = np.expand_dims(frontImage, axis=0)
+#     backImage = np.expand_dims(backImage, axis=0)
+#     tabular_data = np.expand_dims(tabular_data, axis=0)
 
-    return frontImage, backImage, tabular_data
+#     return frontImage, backImage, tabular_data
 
-def get_predictions(frontImage, backImage, weight, height, gender, demographic):
-    front_image, back_image, tabular_data = preprocess(frontImage, backImage, weight, height, gender, demographic)
+# def get_predictions(frontImage, backImage, weight, height, gender, demographic):
+#     front_image, back_image, tabular_data = preprocess(frontImage, backImage, weight, height, gender, demographic)
 
-    predictions_right_bicep, predictions_left_bicep, predictions_chest, predictions_right_forearm, predictions_left_forearm, predictions_right_quad, predictions_left_quad, predictions_right_calf, predictions_left_calf, predictions_waist, predictions_hips, predictions_body_pose = model.predict([front_image, back_image, tabular_data])
+#     predictions_right_bicep, predictions_left_bicep, predictions_chest, predictions_right_forearm, predictions_left_forearm, predictions_right_quad, predictions_left_quad, predictions_right_calf, predictions_left_calf, predictions_waist, predictions_hips, predictions_body_pose = model.predict([front_image, back_image, tabular_data])
     
-    def decode_scalar(vector, output):
-        return np.round(float(s2g[output].decode(vector)), 2) 
+#     def decode_scalar(vector, output):
+#         return np.round(float(s2g[output].decode(vector)), 2) 
     
-    predictions = {
-        'right_bicep': decode_scalar(predictions_right_bicep, 'right_bicep'),
-        'left_bicep': decode_scalar(predictions_left_bicep, 'left_bicep'),
-        'chest': decode_scalar(predictions_chest, 'chest'),
-        'right_forearm': decode_scalar(predictions_right_forearm, 'right_forearm'),
-        'left_forearm': decode_scalar(predictions_left_forearm, 'left_forearm'),
-        'right_quad': decode_scalar(predictions_right_quad, 'right_quad'),
-        'left_quad': decode_scalar(predictions_left_quad, 'left_quad'),
-        'right_calf': decode_scalar(predictions_right_calf, 'right_calf'),
-        'left_calf': decode_scalar(predictions_left_calf, 'left_calf'),
-        'waist': decode_scalar(predictions_waist, 'waist'),
-        'hips': decode_scalar(predictions_hips, 'hips')
-    }
-    return predictions
+#     predictions = {
+#         'right_bicep': decode_scalar(predictions_right_bicep, 'right_bicep'),
+#         'left_bicep': decode_scalar(predictions_left_bicep, 'left_bicep'),
+#         'chest': decode_scalar(predictions_chest, 'chest'),
+#         'right_forearm': decode_scalar(predictions_right_forearm, 'right_forearm'),
+#         'left_forearm': decode_scalar(predictions_left_forearm, 'left_forearm'),
+#         'right_quad': decode_scalar(predictions_right_quad, 'right_quad'),
+#         'left_quad': decode_scalar(predictions_left_quad, 'left_quad'),
+#         'right_calf': decode_scalar(predictions_right_calf, 'right_calf'),
+#         'left_calf': decode_scalar(predictions_left_calf, 'left_calf'),
+#         'waist': decode_scalar(predictions_waist, 'waist'),
+#         'hips': decode_scalar(predictions_hips, 'hips')
+#     }
+#     return predictions
--- scripts/vbc.py
+++ scripts/vbc.py
@@ -1,119 +1,119 @@
-import numpy as np
-import os
-import boto3
-from ultralytics import YOLO
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torchvision import models
-import json
+# import numpy as np
+# import os
+# import boto3
+# from ultralytics import YOLO
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# from torchvision import models
+# import json
 
-model_dir = '/code/files/vbc'
-mean = np.array([0.485, 0.456, 0.406])
-std = np.array([0.229, 0.224, 0.225])
-weights = [1] * 8
-num_folds = 8
+# model_dir = '/code/files/vbc'
+# mean = np.array([0.485, 0.456, 0.406])
+# std = np.array([0.229, 0.224, 0.225])
+# weights = [1] * 8
+# num_folds = 8
 
-class MultiInputModel(nn.Module):
-    def __init__(self, num_tabular_features, outputs):
-        super(MultiInputModel, self).__init__()
+# class MultiInputModel(nn.Module):
+#     def __init__(self, num_tabular_features, outputs):
+#         super(MultiInputModel, self).__init__()
         
-        self.feature_extractor = models.mobilenet_v2(pretrained=True).features
-        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.image_bn = nn.BatchNorm1d(1280)  # 1280 is the number of features from MobileNetV2
-        self.tabular_dense1 = nn.Linear(num_tabular_features, 32)
-        self.tabular_bn = nn.BatchNorm1d(32)
-        combined_features_dim = 1280 * 2 + 32
-        self.output_layers = nn.ModuleList([
-            nn.Linear(combined_features_dim, 1) for _ in outputs
-        ])
+#         self.feature_extractor = models.mobilenet_v2(pretrained=True).features
+#         self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+#         self.image_bn = nn.BatchNorm1d(1280)  # 1280 is the number of features from MobileNetV2
+#         self.tabular_dense1 = nn.Linear(num_tabular_features, 32)
+#         self.tabular_bn = nn.BatchNorm1d(32)
+#         combined_features_dim = 1280 * 2 + 32
+#         self.output_layers = nn.ModuleList([
+#             nn.Linear(combined_features_dim, 1) for _ in outputs
+#         ])
 
-    def process_image(self, x):
-        x = self.feature_extractor(x)
-        x = self.global_avg_pool(x)
-        x = x.view(x.size(0), -1) 
-        x = self.image_bn(x)
-        return x
+#     def process_image(self, x):
+#         x = self.feature_extractor(x)
+#         x = self.global_avg_pool(x)
+#         x = x.view(x.size(0), -1) 
+#         x = self.image_bn(x)
+#         return x
 
-    def forward(self, front_image_input, back_image_input, tabular_input):
-        front_image_features = self.process_image(front_image_input)
-        back_image_features = self.process_image(back_image_input)
+#     def forward(self, front_image_input, back_image_input, tabular_input):
+#         front_image_features = self.process_image(front_image_input)
+#         back_image_features = self.process_image(back_image_input)
         
-        tabular_features = F.relu(self.tabular_dense1(tabular_input))
-        tabular_features = self.tabular_bn(tabular_features)
+#         tabular_features = F.relu(self.tabular_dense1(tabular_input))
+#         tabular_features = self.tabular_bn(tabular_features)
         
-        combined_features = torch.cat([front_image_features, back_image_features, tabular_features], dim=1)
+#         combined_features = torch.cat([front_image_features, back_image_features, tabular_features], dim=1)
         
-        outputs = [output_layer(combined_features) for output_layer in self.output_layers]
+#         outputs = [output_layer(combined_features) for output_layer in self.output_layers]
         
-        return outputs
+#         return outputs
 
-def get_vbc(frontImage, backImage, weight, height, gender, demographic, waist, hips):
-    X_front_images = np.array([((preprocess_image(frontImage).astype(np.float32) / 255) - mean) / std])
-    X_back_images = np.array([((preprocess_image(backImage).astype(np.float32) / 255) - mean) / std])
-    X_tabular = np.array([float(height), float(weight), float(waist) / float(hips)])
-    return ensemble_predict(X_front_images, X_back_images, X_tabular)
+# def get_vbc(frontImage, backImage, weight, height, gender, demographic, waist, hips):
+#     X_front_images = np.array([((preprocess_image(frontImage).astype(np.float32) / 255) - mean) / std])
+#     X_back_images = np.array([((preprocess_image(backImage).astype(np.float32) / 255) - mean) / std])
+#     X_tabular = np.array([float(height), float(weight), float(waist) / float(hips)])
+#     return ensemble_predict(X_front_images, X_back_images, X_tabular)
 
-def ensemble_predict(X_front, X_back, X_tabular):
-    if not os.path.exists(model_dir):
-        load_models()
+# def ensemble_predict(X_front, X_back, X_tabular):
+#     if not os.path.exists(model_dir):
+#         load_models()
 
-    predictions = []
-    for i in range(num_folds):
-        model_path = os.path.join(model_dir, f'model_fold_{i}.pt')
-        model = MultiInputModel(num_tabular_features=3, outputs=[1])
-        model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-        model.eval()
+#     predictions = []
+#     for i in range(num_folds):
+#         model_path = os.path.join(model_dir, f'model_fold_{i}.pt')
+#         model = MultiInputModel(num_tabular_features=3, outputs=[1])
+#         model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+#         model.eval()
 
-        X_front_tensor = torch.tensor(X_front, dtype=torch.float32).permute(0, 3, 1, 2)  # Shape now [1, 3, 224, 224]
-        X_back_tensor = torch.tensor(X_back, dtype=torch.float32).permute(0, 3, 1, 2)  # Shape now [1, 3, 224, 224]
-        X_tabular_tensor = torch.tensor(X_tabular, dtype=torch.float32).unsqueeze(0)  # Shape (1, 3)
+#         X_front_tensor = torch.tensor(X_front, dtype=torch.float32).permute(0, 3, 1, 2)  # Shape now [1, 3, 224, 224]
+#         X_back_tensor = torch.tensor(X_back, dtype=torch.float32).permute(0, 3, 1, 2)  # Shape now [1, 3, 224, 224]
+#         X_tabular_tensor = torch.tensor(X_tabular, dtype=torch.float32).unsqueeze(0)  # Shape (1, 3)
 
-        with torch.no_grad():
-            outputs = model(X_front_tensor, X_back_tensor, X_tabular_tensor)  # This returns a list of outputs
+#         with torch.no_grad():
+#             outputs = model(X_front_tensor, X_back_tensor, X_tabular_tensor)  # This returns a list of outputs
 
-        for output in outputs:
-            predictions_body_fat = output.flatten()  # Flatten each output tensor
-            weighted_pred = predictions_body_fat * weights[i]
-            predictions.append(weighted_pred.cpu().numpy())  # Convert back to numpy for further processing
-        del model
+#         for output in outputs:
+#             predictions_body_fat = output.flatten()  # Flatten each output tensor
+#             weighted_pred = predictions_body_fat * weights[i]
+#             predictions.append(weighted_pred.cpu().numpy())  # Convert back to numpy for further processing
+#         del model
     
-    weighted_avg_prediction = float(np.sum(predictions, axis=0) / sum(weights))
-    rounded_prediction = np.round(weighted_avg_prediction, 2)
+#     weighted_avg_prediction = float(np.sum(predictions, axis=0) / sum(weights))
+#     rounded_prediction = np.round(weighted_avg_prediction, 2)
 
-    response = {
-        'body_fat_percentage': rounded_prediction
-    }
+#     response = {
+#         'body_fat_percentage': rounded_prediction
+#     }
     
-    return response
+#     return response
 
-def load_models():
-    for i in range(num_folds):
-        model_path = os.path.join(model_dir, f'model_fold_{i}.pt')
+# def load_models():
+#     for i in range(num_folds):
+#         model_path = os.path.join(model_dir, f'model_fold_{i}.pt')
         
-        if not os.path.exists(model_path):
-            bucket_name = 'ml-models-kino'
-            file_key = f'vbc/model_fold_{i}.pt'
-            local_file_name = model_path
+#         if not os.path.exists(model_path):
+#             bucket_name = 'ml-models-kino'
+#             file_key = f'vbc/model_fold_{i}.pt'
+#             local_file_name = model_path
 
-            s3 = boto3.client(
-                's3',
-                aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-                aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-            )
-            s3.download_file(bucket_name, file_key, local_file_name)
+#             s3 = boto3.client(
+#                 's3',
+#                 aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+#                 aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+#             )
+#             s3.download_file(bucket_name, file_key, local_file_name)
 
-def preprocess_image(image):
-    try:
-        image = image.convert('RGB')
-        model = YOLO("yolov8n.pt")
-        results = model(image, classes=0)  # or specify custom classes
-        boxes = results[0].boxes
-        coords = boxes.xyxy.tolist()[0]
-        image = image.crop(coords)
-        image = image.resize((224, 224))
-        image_array = np.array(image)
-        return image_array
+# def preprocess_image(image):
+#     try:
+#         image = image.convert('RGB')
+#         model = YOLO("yolov8n.pt")
+#         results = model(image, classes=0)  # or specify custom classes
+#         boxes = results[0].boxes
+#         coords = boxes.xyxy.tolist()[0]
+#         image = image.crop(coords)
+#         image = image.resize((224, 224))
+#         image_array = np.array(image)
+#         return image_array
     
-    except:
-        raise ValueError("image was not able to be preprocessed by YOLO")
+#     except:
+#         raise ValueError("image was not able to be preprocessed by YOLO")
