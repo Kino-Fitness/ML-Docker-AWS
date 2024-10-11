37c3ebccb88e96beb1c85bf900b3d66c75333d76
AdarAvsian <aavsian@gmail.com>
2024-10-10 22:24:45 -0400

    test12

--- initalize.py
+++ initalize.py
@@ -2,33 +2,28 @@ import os
 import boto3
 import torch
 import shutil
-from dotenv import load_dotenv
-load_dotenv()
 
+# Model directories and S3 bucket details
 model_dir = '/code/files/vbc'
+measurements_model_dir = '/code/files/measurements'
+bucket_name = 'ml-models-kino'
 num_folds = 8
 
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-
-if not aws_access_key_id or not aws_secret_access_key:
-    raise EnvironmentError("AWS credentials not found in environment variables.")
-
-# Safely remove the directory if it exists
+# Safely remove the directories if they exist and recreate them
 if os.path.exists(model_dir):
     shutil.rmtree(model_dir)
 os.makedirs(model_dir, exist_ok=True)
 
-s3 = boto3.client(
-    's3',
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-)
+if os.path.exists(measurements_model_dir):
+    shutil.rmtree(measurements_model_dir)
+os.makedirs(measurements_model_dir, exist_ok=True)
 
-# Download the model files from S3
+# Initialize the S3 client without explicit AWS credentials
+s3 = boto3.client('s3')
+
+# Download the model files for each fold from S3
 for i in range(num_folds):
     model_path = os.path.join(model_dir, f'model_fold_{i}.pt')
-    bucket_name = 'ml-models-kino'
     file_key = f'vbc/model_fold_{i}.pt'
     
     try:
@@ -38,14 +33,7 @@ for i in range(num_folds):
         print(f"Error downloading {file_key}: {e}")
 
 # Download the .keras model file from S3
-measurements_model_dir = '/code/files/measurements'
 model_path = os.path.join(measurements_model_dir, 'model.keras')
-
-# Safely remove the directory if it exists
-if os.path.exists(measurements_model_dir):
-    shutil.rmtree(measurements_model_dir)
-os.makedirs(measurements_model_dir, exist_ok=True)
-
 file_key = 'contralateral-measurements/model.keras'
 
 try:
@@ -54,8 +42,10 @@ try:
 except Exception as e:
     print(f"Error downloading {file_key}: {e}")
 
-file_key = 'contralateral-measurements/model.weights.h5'
+# Download the weights file from S3
 weights_path = os.path.join(measurements_model_dir, 'model.weights.h5')
+file_key = 'contralateral-measurements/model.weights.h5'
+
 try:
     print(f"Downloading {file_key} to {weights_path}")
     s3.download_file(bucket_name, file_key, weights_path)
